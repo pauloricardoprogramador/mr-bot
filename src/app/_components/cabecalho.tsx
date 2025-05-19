96bfb292125ea7261740f0a8f378f37d9087b0b9
Paulo Ricardo <programadorpauloricardors@gmail.com>
2025-05-19 14:54:50 -0300

    style: update padding bottom

--- src/app/_components/cabecalho.tsx
+++ src/app/_components/cabecalho.tsx
@@ -5,7 +5,7 @@ interface CabecalhoProps {
 
 export function Cabecalho({ title, description }: CabecalhoProps) {
   return (
-    <div className="flex flex-col items-start space-y-1 border-b border-slate-200 pb-6 mb-4 ">
+    <div className="flex flex-col items-start space-y-1 border-b border-slate-200 pb-4 mb-4 ">
       <h2 className="font-semibold text-2xl text-slate-700">{title}</h2>
       <p className="text-base text-slate-400 ">{description}</p>
     </div>
